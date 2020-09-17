d4c9b67e8f837dbc48bfdafaa38129db03e99638
epaschke22 <ericksonpaschke@gmail.com>
2020-09-17 15:15:52 +0000

    submitting task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - Prints out the alphabet with purchar but not q and e.
+ * Return: should return 0.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' || ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
