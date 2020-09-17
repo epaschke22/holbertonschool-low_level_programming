d991875963860c6b6d1edf1c76aff94720a3b59a
epaschke22 <ericksonpaschke@gmail.com>
2020-09-17 16:39:18 +0000

    fixed spelling in task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
 	if (last > 5)
-		printf("The last digit of %d is %d and is greater than 5\n", n, last);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	else if (last < 6 && last != 0)
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, last);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	else
-		printf("The last digit of %d is %d and is 0\n", n, last);
+		printf("Last digit of %d is %d and is 0\n", n, last);
 	return (0);
 }
