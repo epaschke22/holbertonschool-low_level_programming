736845b2ab150294bc17c355db3fdde1f650d41a
epaschke22 <ericksonpaschke@gmail.com>
2020-09-21 19:46:31 +0000

    submitting task 7

---
+++ 0x02-function_nexted_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+#include "holberton.h"
+
+int main(void)
+{
+	int r;
+
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
+	return (0);
+}
