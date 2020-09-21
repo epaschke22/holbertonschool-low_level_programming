1ac7a11d215ec74d0b9ec81c17630094169400f8
epaschke22 <ericksonpaschke@gmail.com>
2020-09-21 16:41:37 +0000

    submitting task 1

---
+++ 0x02-function_nexted_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints the alphabet with the print_alphabet function
+ * Return: should always be 0
+ */
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
