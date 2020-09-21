e20c3fe3c6cbaa3cd585bac3709913602d96baee
epaschke22 <ericksonpaschke@gmail.com>
2020-09-21 20:13:23 +0000

    updating holberton.h

--- 0x02-function_nexted_loops/holberton.h
+++ 0x02-function_nexted_loops/holberton.h
@@ -21,6 +21,10 @@
  *
  * _abs - prints absolute value of int
  *
+ * print_last_digit - prints last digit of a number
+ *
+ * jack_bauer - prints every minute for 24 hours from 00:00 to 23:59
+ *
  */
 int _putchar(char c)
 {
@@ -113,5 +117,39 @@ int _abs(int n)
 		result = 0;
 	}
 	return result;
+}
+
+int print_last_digit(int n)
+{
+	int result = _abs(n % 10);
+	_putchar('0' + result);
+	return result;
+}
+
+void jack_bauer(void)
+{
+	int minuteOnes;
+	int minuteTens;
+	int hourOnes;
+	int hourTens;
+
+	for (hourTens = 0; hourTens < 3; hourTens++)
+	{
+		for (hourOnes = 0; hourOnes < 4; hourOnes++)
+		{
+			for (minuteTens = 0; minuteTens < 6; minuteTens++)
+			{
+				for (minuteOnes = 0; minuteOnes < 10; minuteOnes++)
+				{
+					_putchar(hourTens + '0');
+					_putchar(hourOnes + '0');
+					_putchar(':');
+					_putchar(minuteTens + '0');
+					_putchar(minuteOnes + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
 # endif /*HEADER_H*/
