b3183bac27413347b3d2348e19725e615a6a3560
epaschke22 <ericksonpaschke@gmail.com>
2020-09-21 19:15:52 +0000

    submitting task 5

---
+++ 0x02-function_nexted_loops/5-sign.c
@@ -0,0 +1,33 @@
+/**
+ * main - prints the sign of a number with print_sign
+ * Return: always 0.
+ */
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void)
+{
+	int r;
+
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
