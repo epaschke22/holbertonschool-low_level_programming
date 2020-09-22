0fe63076f9105c8a1f858b7b3e9ae6844606ef2c
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 13:47:52 +0000

    updating task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,18 @@
+#include "holberton.h"
 /**
- * main - _isalpha returns 1 if argument is a letter, else 0
+ * _isalpha - _isalpha returns 1 if argument is a letter, else 0
+ * @c: is argument to check for if alphabet.
  * Return: always 0.
  */
-#include <stdio.h>
-#include "holberton.h"
 
-int main(void)
+int _isalpha(int c)
 {
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
