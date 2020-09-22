33d4c4b3b3b949491ebda4e1ce4be518c679458b
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 13:22:22 +0000

    updating task 0

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -32,16 +32,16 @@
  * print_to_98 - prints all natrual numbers from n to 98.
  *
  */
-int _putchar(char c)
-void print_alphabet(void)
-void print_alphabet_x10(void)
-int _islower(int c)
-int _isalpha(int c)
-int print_sign(int n)
-int _abs(int n)
-int print_last_digit(int n)
-void jack_bauer(void)
-void times_table(void)
-int add(int, int)
-void print_to_98(int)
+int _putchar(char c);
+void print_alphabet(void); 
+void print_alphabet_x10(void);   
+int _islower(int c);     
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int);
 #endif /*HEADER_H*/
