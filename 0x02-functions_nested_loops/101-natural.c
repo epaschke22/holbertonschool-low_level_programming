40c658c02dc2706bf75ccba0df9faf8cc93853e8
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 21:08:55 +0000

    submitting advanced task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints out the sum of all mutiples of 3 and 5 below 1024
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	int totalsum;
+	int multiple;
+
+	for (multiple = 0; multiple < 1025; multiple++)
+		if ((multiple % 3) == 0 || (multiple % 5) == 0)
+			totalsum = totalsum + multiple;
+	printf("%d\n",totalsum);
+	return (0);
+}
