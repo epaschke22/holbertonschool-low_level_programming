e4e72ea37c8b7dde000a547083774493570162fe
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 13:39:19 +0000

    updating task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,20 @@
+#include "holberton.h"
 /**
- * main - print out the alphabet 10 times
+ * print_alphabet_x10 - print out the alphabet 10 times
  * Return: always 0.
  */
-#include <stdio.h>
-#include "holberton.h"
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
