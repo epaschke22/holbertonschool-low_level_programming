4ba5946e7f314ee7e6b6c6ef2a34da2c92aa8579
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 20:47:05 +0000

    updating task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,6 @@ void print_times_table(int number)
 	int times1;
 	int times2;
 	int product;
-	int middle;
 
 	if (number >= 0 && number <= 15)
 		for (times1 = 0; times1 <= number; times1++)
@@ -19,30 +18,28 @@ void print_times_table(int number)
 				product = times1 * times2;
 				if (times2 == 0)
 					_putchar('0');
-				else if (product < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
-				}
-				else if (product >= 10 && product < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product / 10 + '0');
-					_putchar(product % 10 + '0');
-				}
 				else
 				{
-					middle = (product / 10) % 10;
 					_putchar(',');
 					_putchar(' ');
-					_putchar(product / 100 + '0');
-					_putchar(middle + '0');
-					_putchar(product % 10 + '0');
+					if (product < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(product + '0');
+					}
+					else if (product >= 10 && product < 100)
+					{
+						_putchar(' ');
+						_putchar(product / 10 + '0');
+						_putchar(product % 10 + '0');
+					}
+					else
+					{
+						_putchar(product / 100 + '0');
+						_putchar(((product / 10) % 10) + '0');
+						_putchar(product % 10 + '0');
+					}
 				}
 			}
 			_putchar('\n');
