5f84708deef0bb21c598656818904d5027952113
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 14:17:53 +0000

    updating task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 	int minuteTens;
 	int hour;
 
-	for (hour = 0; hour < 23; hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (minuteTens = 0; minuteTens < 6; minuteTens++)
 		{
