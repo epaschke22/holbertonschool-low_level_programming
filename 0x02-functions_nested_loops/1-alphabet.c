9e79101e224aaaebaf779948e71cbe4bf49aacba
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 13:26:36 +0000

    updating task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 #include "holberton.h"
 
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
+
 int main(void)
 {
 	print_alphabet();
