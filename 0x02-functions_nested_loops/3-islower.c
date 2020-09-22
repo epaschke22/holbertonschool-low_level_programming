2a40c7b94d3f3ceec97b52bba95aefff497637d3
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 13:44:00 +0000

    updating task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
+#include "holberton.h"
 /**
- * main - prints 0 or 1 for if r is a lower case letter.
+ * _islower - prints 0 or 1 for if r is a lower case letter.
+ * @c: checks this argument if it is a lowercase letter.
  * Return: always 0.
  */
-#include <stdio.h>
-#include "holberton.h"
 
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
