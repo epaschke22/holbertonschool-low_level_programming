eb9ae49f5068a994f7e8f392762245603ebbace0
epaschke22 <ericksonpaschke@gmail.com>
2020-09-22 15:04:27 +0000

    updatting task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,41 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * times_table - prints out the times tables for 0 to 9.
  *
  * Return: Always 0.
  */
-int main(void)
+
+void times_table(void)
 {
-	times_table();
-	return (0);
+	int times1;
+	int times2;
+	int product = times1 * times2;
+
+	for (times1 = 0; times1 < 10; times1++)
+	{
+		for (times2 = 0; times2 < 10; times2++)
+		{
+			product = times1 * times2;
+			if (times2 == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
