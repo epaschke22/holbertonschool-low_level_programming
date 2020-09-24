548898abf5cd6ba73702e9f8b615c73d1d5389a2
epaschke22 <ericksonpaschke@gmail.com>
2020-09-24 13:55:46 +0000

    submitting task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints out numbers 0 to 14 ten times.
+ * Return: always 0.
+ */
+
+void more_numbers(void)
+{
+	int number;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (number = 0; number < 15; number++)
+		{
+			if (number < 9)
+				_putchar(number + '0');
+			else
+			{
+				_putchar(number / 10 + '0');
+				_putchar(number % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
