081c96a03b08450a4125de1a9cebe5ad6856e6b2
epaschke22 <ericksonpaschke@gmail.com>
2020-09-28 14:10:51 +0000

    submitting first answer and readme of project 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - changes the pointer of n to 98.
+ * Return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+These are the submitions for project 0x05-pointers arrays and strings.
