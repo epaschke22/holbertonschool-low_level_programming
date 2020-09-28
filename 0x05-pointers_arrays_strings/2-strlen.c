623f2cb857c7834f6b8d8d482465a4b3652856ee
epaschke22 <ericksonpaschke@gmail.com>
2020-09-28 16:55:55 +0000

    submitting task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen - returns string length.
+ * @s: string to calculate.
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		*s++;
+	}
+	return (length);
+}
