218ba8f48ca2220b680204fd3bc79f87b5f5f6ad
epaschke22 <ericksonpaschke@gmail.com>
2020-09-29 17:44:09 +0000

    submitting task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strcpy - copies the string pointed to by src.
+ * @dest: destination of the copied string
+ * @src: the source of the string to be copied.
+ * Return: always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
