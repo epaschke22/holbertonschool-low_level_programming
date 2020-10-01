275cfbfddecc3a29bfb2b1087aab29569236279a
epaschke22 <ericksonpaschke@gmail.com>
2020-10-01 17:22:44 +0000

    updating task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = 0, len2 = 0, i;
+	int i;
 
 	for (i = 0; s1[i] != 0; i++)
-		len1++;
-	for (i = 0; s2[i] != 0; i++)
-		len2++;
-
-	if (len1 > len2)
-		return (15);
-	else if (len1 == len2)
-		return (0);
-	else
-		return (-15);
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		else
+			return (0);
+	}
 }
