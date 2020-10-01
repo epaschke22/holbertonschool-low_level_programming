3401e3d9b0ccaaa6ba5d3899b285d7bc4be3e7c0
epaschke22 <ericksonpaschke@gmail.com>
2020-10-01 19:09:06 +0000

    updating task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,13 @@ char *cap_string(char *s)
 	int i, j;
 	char ends[] = " \t\n,;.!?\"(){}";
 
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -=32;
+
 	for (i = 0; s[i] != 0; i++)
-	{
 		for (j = 0; ends[j] != 0; j++)
 			if (s[i - 1] == ends[j])
-			    if (s[i] >= 'a' && s[i] <= 'z')
-				    s[i] -= 32;
-	}
+				if (s[i] >= 'a' && s[i] <= 'z')
+					s[i] -= 32;
 	return (s);
 }
