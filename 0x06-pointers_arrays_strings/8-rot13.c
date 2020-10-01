26d64aa47db9a1069bd4584179f7eb9dacc613f4
epaschke22 <ericksonpaschke@gmail.com>
2020-10-01 20:51:24 +0000

    updating task 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -12,8 +12,11 @@ char *rot13(char *s)
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != 0; i++)
-		for (j = 0; alpha[j] = 0; j++)
+		for (j = 0; alpha[j] != 0; j++)
 			if (s[i] == alpha[j])
+			{
 				s[i] = rot13[j];
+				break;
+			}
 	return (s);
 }
