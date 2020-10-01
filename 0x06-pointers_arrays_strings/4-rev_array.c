36bd87d2e8fdf7a2f65b3dcbeda4635962420f72
epaschke22 <ericksonpaschke@gmail.com>
2020-10-01 15:14:01 +0000

    submitting task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverses an array.
+ * @a: the array to reverse.
+ * @n: the number of elemtns in the array.
+ * Return: always 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, i;
+
+	for (i = 0; i <= n; i++)
+	{
+		n--;
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
