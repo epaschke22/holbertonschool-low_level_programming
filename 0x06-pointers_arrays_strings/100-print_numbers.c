d4ad829b8175b581f11ca8728fd7fb0b62f05794
epaschke22 <ericksonpaschke@gmail.com>
2020-10-02 18:38:59 +0000

    submitting advanced task 9

---
+++ 0x06-pointers_arrays_strings/100-print_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _printnumber - prints out a number
+ * Return: always 0
+ */
+
+void print_number(int n)
+{
+	int place = 1;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	while (n / place >= 10)
+		place *= 10;
+	while (place != 0)
+	{
+		_putchar((n / place) % 10 + '0');
+		place /= 10;
+	}
+}
