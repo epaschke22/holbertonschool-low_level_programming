28a1db408bb85f7f8f2eb0457e93858a4031aab2
epaschke22 <ericksonpaschke@gmail.com>
2020-10-05 16:54:51 +0000

    updating task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
+	if (c == 0)
+		return (0);
 	for (i = 0; s[i] != 0; i++)
 		if (s[i] == c)
+		{
+			return (s + i);
 			break;
-	if (s[i] == 0)
-		return (0);
-	return (s + i);
+		}
+	return (0);
 }
