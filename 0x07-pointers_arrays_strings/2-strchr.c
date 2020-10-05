eed9b3b76e97ade5d2c5f92156cdf6a7f0edfc57
epaschke22 <ericksonpaschke@gmail.com>
2020-10-05 16:37:20 +0000

    submitting task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strchr - finds and returns the character in a string.
+ * @s: string to look in.
+ * @c: character to look for.
+ * Return: pointer to found letter of s.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+		if (s[i] == c)
+			break;
+       	return (s + i);
+
+	if (s[i] == 0)
+		return (0);
+}
