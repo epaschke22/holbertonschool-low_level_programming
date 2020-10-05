3ededd38e579aa35b8dd97001ff39b7e076f6a5c
epaschke22 <ericksonpaschke@gmail.com>
2020-10-05 19:02:44 +0000

    submitting task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strstr - finds string within another string.
+ * @haystack: the string to look through.
+ * @needle: the string to return.
+ * Return: pointer of substring in haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int length, i, j, k, place;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			place = i;
+			k = i;
+			for (j = 0; needle[j]; j++)
+				if (needle[j] == haystack[k])
+				{
+					k++;
+				}
+				else
+					place = 0;
+		}		
+	}
+	return (haystack + place);
+}
