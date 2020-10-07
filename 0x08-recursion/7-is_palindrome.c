a896b4bea3cd70ade5f1678147a587854e05964b
epaschke22 <ericksonpaschke@gmail.com>
2020-10-07 19:33:41 +0000

    submitting task 7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string.
+ * Return: length of string.
+ */
+
+int _strlen(char *s)
+{
+	if (*s++)
+		return (1 + _strlen(s));
+	else
+		return (0);
+}
+
+/**
+ * bothends - checks both ends of the string's characters;
+ * @s: string to reverse.
+ * @begin: starting point of string.
+ * @end: length of string.
+ * Return: 0 if not equal, else 1.
+ */
+int bothends(int begin, int end, char *s)
+{
+	if (*s)
+	{
+		if (s[begin] != s[end])
+			return (0);
+		bothends(begin + 1, end - 1, s);
+	}
+	return (1);
+}
+
+/**
+ * is_palindrome - returns weather s is a palindrone or not.
+ * @s: input string.
+ * Return: 1 if palindrome else 0.
+ */
+int is_palindrome(char *s)
+{
+	return (bothends(0, _strlen(s) - 1, s));
+}
