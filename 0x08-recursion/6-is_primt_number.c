9490ad2c1035193984f62bf12d9dad6ead9004c6
epaschke22 <ericksonpaschke@gmail.com>
2020-10-07 16:55:21 +0000

    submitting task 6 and updating task 1

---
+++ 0x08-recursion/6-is_primt_number.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - returns weather a number is prime.
+ * @n: input number.
+ * Return: 1 if prime, else 0.
+ */
+int is_prime_number(int n)
+{
+	return (0);
+}
