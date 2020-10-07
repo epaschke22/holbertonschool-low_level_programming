3266ae286223ec527d94bbc0a4130b924d1b0b9c
epaschke22 <ericksonpaschke@gmail.com>
2020-10-07 16:47:23 +0000

    submitting task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * helper - helps find sqrt of n using a.
+ * @n: number input.
+ * @a: number from 1 to n to check sqrt.
+ * Return: either a or -1.
+ */
+
+int helper(int n, int a)
+{
+	if (a * a == n)
+		return (a);
+	if (a * a > n)
+		return (-1);
+	return (helper(n, a + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: input number
+ * Return: return sqrt of n, if n not natrual, return -1.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (helper(n, 1));
+}
