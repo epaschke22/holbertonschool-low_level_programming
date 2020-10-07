046631af21175f421a12d10235aa8c38981dd110
epaschke22 <ericksonpaschke@gmail.com>
2020-10-07 17:23:14 +0000

    submitting task 6

--- 0x08-recursion/6-is_primt_number.c
+++ 0x08-recursion/6-is_primt_number.c
@@ -1,11 +1,30 @@
 #include "holberton.h"
 
 /**
+ * helper - helps find if n is prime of n using a.
+ * @n: number input.
+ * @a: number from 1 to n to check sqrt.
+ * Return: either a or -1.
+ */
+int helper(int n, int a)
+{
+	if (n % a == 0)
+		return (0);
+	if (a > n / 2)
+		return (1);
+	return (helper(n, a + 1));
+}
+
+/**
  * is_prime_number - returns weather a number is prime.
  * @n: input number.
  * Return: 1 if prime, else 0.
  */
 int is_prime_number(int n)
 {
-	return (0);
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (helper(n, 2));
 }
