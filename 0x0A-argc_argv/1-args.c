4446d68227691fcd7c4e75fa640585cbc13a7482
epaschke22 <ericksonpaschke@gmail.com>
2020-10-09 14:37:23 +0000

    submitting task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - print out the argument count.
+ * @argc: argument count.
+ * @argv: argument vector array.
+ * Return: always 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
