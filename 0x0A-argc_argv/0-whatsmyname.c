ee404ee4b2e269eda409911a01b2054fcee7fb3f
epaschke22 <ericksonpaschke@gmail.com>
2020-10-11 23:36:05 +0000

    updating task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,10 @@
  * @argv: argument vector array.
  * Return: always 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i = argc;
+
+	printf("%s\n", argv[i - 1]);
 	return (0);
 }
