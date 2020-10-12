94bee7514d62cd80cf3dc4b0f0409f9a6da8cce0
epaschke22 <ericksonpaschke@gmail.com>
2020-10-12 20:00:52 +0000

    updating task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - outputs message without puts or printf, and prints it to error output.
+ * Return: prints out a message and return 1.
+ */
+int main(void)
+{
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return 0;
+}
