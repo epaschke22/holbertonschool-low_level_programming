5b1ff8cc0532970beff05ccbe39a88577ec8c6d4
epaschke22 <ericksonpaschke@gmail.com>
2020-10-13 21:09:51 +0000

    updating task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,10 +11,7 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (height == 0)
-		return (NULL);
-	if (grid == NULL)
-		return (NULL);
 	for (i = 0; i < height; i++)
 		free(grid[i]);
+	free(grid);
 }
