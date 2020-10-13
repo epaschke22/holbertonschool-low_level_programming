469e0de59393715cfa264cdde3ca10ede68006d9
epaschke22 <ericksonpaschke@gmail.com>
2020-10-13 15:32:06 +0000

    submitting task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strcpy - copies one string from another.
+ * @dest: result to copy too.
+ * @src: string to copy from.
+ * Return: the copied pointer string.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string to copy
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, strlen = 0;
+	char *ar;
+
+	if (str == 0)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		strlen++;
+
+	ar = malloc((strlen + 1) * sizeof(char));
+	_strcpy(ar, str);
+
+	return (ar);
+}
