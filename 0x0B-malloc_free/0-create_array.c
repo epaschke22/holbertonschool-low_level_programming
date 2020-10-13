3c4349794db2bd8fcec37f8647be94e329491bf8
epaschke22 <ericksonpaschke@gmail.com>
2020-10-13 17:36:39 +0000

    updating task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,13 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	unsigned int i;
 
-	if (size == 0 || c == 0)
+	if (size == 0)
 		return (NULL);
 
 	ar = malloc(size * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		ar[i] = c;
