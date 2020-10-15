cef51eea31df79a7c833e2a1efa8b7924e888961
epaschke22 <ericksonpaschke@gmail.com>
2020-10-15 16:44:58 +0000

    updating task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,6 +5,7 @@
 /**
  * malloc_checked - allocates memory using malloc.
  * @b: btye sisze to allocate.
+ * Return: malloc array.
  */
 void *malloc_checked(unsigned int b)
 {
@@ -12,6 +13,9 @@ void *malloc_checked(unsigned int b)
 
 	ar = malloc(b);
 	if (ar == NULL)
+	{
+		free(ar);
 		exit(98);
+	}
 	return (ar);
 }
