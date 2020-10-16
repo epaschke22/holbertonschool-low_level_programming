e15518fd8ea491f4fb9bfbdbe6bf7cfd7f0ed906
epaschke22 <ericksonpaschke@gmail.com>
2020-10-16 17:55:32 +0000

    updating task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 
-	in (new_size < old_size)
+	if (new_size < old_size)
 		old_size = new_size;
 
 	temp = malloc(new_size);
