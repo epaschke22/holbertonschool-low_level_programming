0b0b02a6d1eea27fdddd777e88288f5dbf23f61f
epaschke22 <ericksonpaschke@gmail.com>
2020-10-19 17:57:34 +0000

    submitting task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,14 @@
-something
+#include "dog.h"
+/**
+ * init_dog - initializes the values for struct dog
+ * @d: dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
