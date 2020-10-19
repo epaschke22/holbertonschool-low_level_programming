2115f5cb01246e8b7fc84eef38e4f7d41239fb4b
epaschke22 <ericksonpaschke@gmail.com>
2020-10-19 17:37:24 +0000

    updating task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a structure of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
