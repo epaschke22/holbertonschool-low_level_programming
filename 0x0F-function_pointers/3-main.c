91940f1a6a0f50332be2f7d9299b4c6d3b8957f0
epaschke22 <ericksonpaschke@gmail.com>
2020-10-22 02:25:55 +0000

    updating task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,11 +12,6 @@
 int main(int argc, char *argv[])
 {
 	int result, n1, n2;
-	char *s;
-
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	s = argv[2];
 
 	if (argc != 4)
 	{
@@ -24,19 +19,20 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (get_op_func(s) == NULL || argv[2][1] != '\0')
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
 	if ((argv[2][0] == '/' && n2 == 0) || (argv[2][0] == '%' && n2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = (get_op_func(s))(n1, n2);
+	result = (get_op_func(argv[2]))(n1, n2);
 
 	printf("%d\n", result);
 	return (0);
