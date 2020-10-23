73fd58e40d93229ecb6e51a7e6e1ed50539bc16a
epaschke22 <ericksonpaschke@gmail.com>
2020-10-23 18:15:06 +0000

    submitting task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints out chars, ints, floats, and strings
+ * @format: a seperator between numbers
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	int len = 0, i = 0, flag = 0;
+	char *s;
+
+	while (format[len])
+		len++;
+	va_start(valist, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(valist, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double));
+			break;
+		case 's':
+			s = va_arg(valist, char *);
+			if (s == NULL)
+			{
+				printf("%p", s);
+				break;
+			}
+			printf("%s", s);
+			break;
+		default:
+			flag = 1;
+			break;
+		}
+		if (i < len - 1 && flag == 0)
+			printf(", ");
+		flag = 0;
+		i++;
+	}
+	putchar('\n');
+	va_end(valist);
+}
