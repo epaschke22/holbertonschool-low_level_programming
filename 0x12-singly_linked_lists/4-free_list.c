33eb891780127a1d5a8dcda2bf218b7d14e1631e
epaschke22 <ericksonpaschke@gmail.com>
2020-10-29 18:49:26 +0000

    submitting task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees memory of each item in a list
+ * @head: list to free
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
