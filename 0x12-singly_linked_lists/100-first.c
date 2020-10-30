bc7e15d82559331de9f0c118f2ad90b647afa926
epaschke22 <ericksonpaschke@gmail.com>
2020-10-30 22:30:12 +0000

    submitting advanced task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * first - prints a message before main is compiled.
+ * Return: always 0
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
