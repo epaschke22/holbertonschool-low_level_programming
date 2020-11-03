d8467be70ae82b22f98a1a45cef1435f64c0f851
epaschke22 <ericksonpaschke@gmail.com>
2020-11-03 20:41:48 +0000

    updating task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,6 +3,23 @@
 #include "lists.h"
 
 /**
+ * listint_len - returns the number of elements in linked
+ * @h: list to prtinf from
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+/**
  * free_listint2 - frees memory of list and sets head to null
  * @head: list to free
  * Return: void
@@ -10,10 +27,13 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp, *tmphead;
+	int len = 0;
 
-	if (*head == NULL)
-		return;
 	tmphead = *head;
+	len = (listint_len(tmphead));
+
+	if (len == 0)
+		return;
 	while (tmphead != NULL)
 	{
 		temp = tmphead->next;
