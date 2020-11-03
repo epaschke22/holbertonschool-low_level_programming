4830bbb27f30cf8a2f87a0605c59cdd9e7ec8ed9
epaschke22 <ericksonpaschke@gmail.com>
2020-11-03 15:13:33 +0000

    updating task 9 with functions included

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -62,7 +62,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	len = listint_len(tmphead);
 	if (idx == 0)
 		return (add_nodeint(head, n));
-	if (idx == len)
+	if (idx == len - 1)
 		return (add_nodeint_end(head, n));
 	while (tmphead != NULL)
 	{
