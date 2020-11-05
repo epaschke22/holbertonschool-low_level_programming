ace12c27b5a12b919d347e437c9da85630bb7f48
epaschke22 <ericksonpaschke@gmail.com>
2020-11-05 16:01:08 +0000

    submitting project files and task 0

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a string of 1s and 0s to a decimal
+ * @b: input string
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, power = 1, number = 0;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		power *= 2;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i]; i++)
+	{
+		power /= 2;
+		if (b[i] == '1')
+			number += power;
+	}
+	return (number);
+}
