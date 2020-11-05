f21ffa56e5d6b4fde0c386966a3328494e575b73
epaschke22 <ericksonpaschke@gmail.com>
2020-11-05 18:53:47 +0000

    updating task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,23 +10,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int count = 0, blen = 0;
-	unsigned long int tn;
-
-	tn = n;
-	if (n == 0)
-		return (0);
-	while (tn > 0)
-	{
-		tn = tn >> 1;
-		blen++;
-	}
-	if (index > blen)
+	if (index > 63)
 		return (-1);
-	while (index > count)
-	{
-		n = n >> 1;
-		count++;
-	}
-	return (n & 1);
+
+	return (n >> index & 1);
 }
