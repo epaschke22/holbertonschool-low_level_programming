808c85d919c40e70848cf2421af3e72ff7607d2a
epaschke22 <ericksonpaschke@gmail.com>
2020-11-09 18:52:38 +0000

    updating task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,16 +19,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[len])
-		len++;
 	file = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (file == -1)
 		return (-1);
-	if (len > 0)
+
+	if (text_content != NULL)
 	{
-		written = write(file, text_content, len);
-		if (written == -1)
-			return (-1);
+		while (text_content[len])
+			len++;
+		if (len > 0)
+		{
+			written = write(file, text_content, len);
+			if (written == -1)
+				return (-1);
+		}
 	}
 	return (1);
 }
