6c9bddbfc7139fce3a6e9d6be49d8941076b44a7
epaschke22 <ericksonpaschke@gmail.com>
2020-12-14 04:01:49 +0000

    updating task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	while (last->next != NULL)
 		last = last->next;
-	new->prev = last;
 	last->next = new;
+	new->prev = last;
 	return (*head);
 }
