232341b2bfafaab53adfe2a1f4b499a3a4b2bc67
epaschke22 <ericksonpaschke@gmail.com>
2020-12-14 03:34:59 +0000

    updating task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,23 +3,6 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in linked
- * @h: list to prtinf from
- * Return: number of elements
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	int count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-/**
  * insert_dnodeint_at_index - returns the nth node of a list
  * @h: list to look through
  * @idx: the place to add the node
@@ -34,20 +17,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-
-	len = dlistint_len(tmphead);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	if (idx == len)
-		return (add_dnodeint_end(h, n));
+		
 	while (tmphead != NULL)
 	{
 		if (count == idx)
 		{
+			new = malloc(sizeof(dlistint_t));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
 			new->next = tmphead;
 			new->prev = tmphead->prev;
 			tmphead->prev->next = new;
@@ -57,5 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 		tmphead = tmphead->next;
 	}
+	if (idx == count)
+		return (add_dnodeint_end(h, n));
 	return (NULL);
 }
