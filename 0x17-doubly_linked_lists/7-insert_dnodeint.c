1918bdfc5fd30e9195f215902ca5ade28b690e07
epaschke22 <ericksonpaschke@gmail.com>
2020-12-14 03:51:52 +0000

    updating task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
-		return (add_dnodeint(h, n));
+		return (add_dnodeint(&*h, n));
 	while (tmphead != NULL)
 	{
 		if (count == idx)
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 		tmphead = tmphead->next;
 	}
-	if (idx > count)
+	if (idx > count)	
 		return (NULL);
-	return (add_dnodeint_end(h, n));
+	return (add_dnodeint_end(&*h, n));
 }
