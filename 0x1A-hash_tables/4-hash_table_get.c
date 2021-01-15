06fd753dedc8a86da7f93c6dcbc99a376f87c319
epaschke22 <ericksonpaschke@gmail.com>
2021-01-15 18:17:47 +0000

    updating task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,16 +10,20 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
 
+
+	if (strcmp("", (char *)key) == 0 || ht == NULL)
+		return (NULL);
 	node = ht->array[hash_djb2((unsigned char *)key) % ht->size];
 	if (node == NULL)
 		return (NULL);
 	if (strcmp(node->key, (char *)key) == 0)
 		return (node->value);
-	while (node->next != NULL)
-	{
-	if (strcmp(node->key, (char *)key) == 0)
-		return (node->value);
-	node = node->next;
-	}
+	if (node->next != NULL)
+		while (node->next != NULL)
+		{
+			if (strcmp(node->key, (char *)key) == 0)
+				return (node->value);
+			node = node->next;
+		}
 	return (NULL);
 }
