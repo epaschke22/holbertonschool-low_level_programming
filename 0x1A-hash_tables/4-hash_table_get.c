a618003d4a3e2131807a7f22dbd1a68b2af19ed3
epaschke22 <ericksonpaschke@gmail.com>
2021-01-15 15:22:47 +0000

    fixed betty issues task 2, 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,18 +8,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *node;
+	hash_node_t *node;
 
-    node = ht->array[hash_djb2((unsigned char *)key) % ht->size];
-    if (node == NULL)
-        return (NULL);
-    if (strcmp(node->key, (char *)key) == 0)
-        return (node->value);
-    while(node->next != NULL)
-    {
-        if (strcmp(node->key, (char *)key) == 0)
-            return (node->value);
-        node = node->next;
-    }
-    return (NULL);
+	node = ht->array[hash_djb2((unsigned char *)key) % ht->size];
+	if (node == NULL)
+		return (NULL);
+	if (strcmp(node->key, (char *)key) == 0)
+		return (node->value);
+	while (node->next != NULL)
+	{
+	if (strcmp(node->key, (char *)key) == 0)
+		return (node->value);
+	node = node->next;
+	}
+	return (NULL);
 }
