a4467555b53e0e09d4d069c25dfffa993d9564ae
epaschke22 <ericksonpaschke@gmail.com>
2021-01-15 19:34:01 +0000

    updating task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL ||
 		ht->size == 0 || ht->array == NULL || key[0] == '\0')
-		return (1);
+		return (0);
 	index = hash_djb2((unsigned char *)key) % ht->size;
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
