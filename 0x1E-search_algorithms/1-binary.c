9c2dc59748df684a136f74215def78f77fa9adbb
epaschke22 <ericksonpaschke@gmail.com>
2021-04-19 18:13:41 +0000

    submitting task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * recursive - searches through an array like a binary tree
+ * @array: input array
+ * @left: left most index
+ * @right: right most index
+ * @value: value to search for
+ * Return: index where value is stored, else -1
+ */
+int recursive(int *array, int left, int right, int value)
+{
+	int mid, i;
+
+	if (right >= left)
+	{
+		/* print array */
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+			printf("%i%s", array[i], i <= right - 1 ? ", " : "");
+		printf("\n");
+
+		mid = left + (right - left) / 2;
+		if (array[mid] == value)
+			return mid;
+		if (array[mid] > value)
+			return recursive(array, left, mid - 1, value);
+		return recursive(array, mid + 1, right, value);
+	}
+	return (-1);
+}
+
+/**
+ * binary_search - searches through an array like a binary tree
+ * @array: input array
+ * @size: size of input array
+ * @value: value to search for
+ * Return: index where value is stored, else -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return(recursive(array, 0, size - 1, value));
+}
