115b2da7b192123aa44b73db7654beacbdb51cd6
epaschke22 <ericksonpaschke@gmail.com>
2021-04-19 18:11:25 +0000

    submitting task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches through an array linealy
+ * @array: input array
+ * @size: size of input array
+ * @value: value to search for
+ * Return: index where value is stored, else -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%u] = [%i]\n", i, array[i]);
+		if (value == array[i])
+			return i;
+	}
+	return (-1);
+}
